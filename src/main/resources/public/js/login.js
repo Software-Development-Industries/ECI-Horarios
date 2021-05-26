dad721a65cc666d9c6cf7eeeae68453f15fe7a26
Eduard Arias <eduard.arias@mail.escuelaing.edu.co>
2021-05-26 11:03:20 -0400

    .js modificados

--- src/main/resources/public/js/login.js
+++ src/main/resources/public/js/login.js
@@ -2,7 +2,7 @@ login = (function (){
 	
 	
 	var port = 80;
-	var url = "https://localhost:"+port;
+	var url = "https://eci-horarios.herokuapp.com:"+port;
 	
 	var _studentLogin = async function(email, pass) {
 		let isLoggued = false;
